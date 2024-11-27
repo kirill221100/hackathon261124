b2eeb71793df01df77eb59be57190b820d3e9ec3
kirill221100 <kirulkacool@gmail.com>
2024-11-27 11:02:13 +0700

    fix

--- db/utils/document.py
+++ db/utils/document.py
@@ -7,5 +7,5 @@ from db.models.document import Document
 async def create_document(text: str, name: str, session: AsyncSession):
     doc = Document(name=name, text=text)
     session.add(doc)
-    await session.commit()
+    #await session.commit()
     return doc
--- utils/document.py
+++ utils/document.py
@@ -3,6 +3,8 @@ from pypdf import PdfReader
 from docx import Document
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
+from typing import List
+from fastapi import UploadFile, HTTPException
 from db.utils.document import create_document
 from fastapi.concurrency import run_in_threadpool
 
@@ -27,19 +29,32 @@ def read_docx_text(file: BytesIO):
     return text
 
 
-# def read_txt(file: BytesIO):
+async def upload_docs_func(docs: List[UploadFile], session: AsyncSession):
+    res = []
+    for doc in docs:
+        if doc.content_type not in ('application/pdf', 'text/plain',
+                                    'application/vnd.openxmlformats-officedocument.wordprocessingml.document'):
+            raise HTTPException(400, f"Тип файла {doc.filename} не поддерживается")
+    for doc in docs:
+        if doc.content_type == 'application/pdf':
+            res.append(await create_pdf(doc, session))
+        elif doc.content_type == 'text/plain':
+            res.append(await create_document((await doc.read()).decode("utf-8"), doc.filename, session))
+        elif doc.content_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+            res.append(await create_docx(doc, session))
+    await session.commit()
+    return res
 
 
 
-async def create_pdf_func(file: BytesIO, name: str, session: AsyncSession):
-    text = await run_in_threadpool(read_pdf_text, file)
-    return await create_document(text, name, session)
+
+async def create_pdf(file: UploadFile, session: AsyncSession):
+
+    text = await run_in_threadpool(read_pdf_text, BytesIO(await file.read()))
+    return await create_document(text, file.filename, session)
 
 
-async def create_docx_func(file: BytesIO, name: str, session: AsyncSession):
-    text = await run_in_threadpool(read_docx_text, file)
-    return await create_document(text, name, session)
+async def create_docx(file: UploadFile, session: AsyncSession):
+    text = await run_in_threadpool(read_docx_text, BytesIO(await file.read()))
+    return await create_document(text, file.filename, session)
 
-
-# async def create_txt_func(file, name: str, session: AsyncSession):
-#     return await create_document(text, name, session)
